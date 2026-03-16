9bc89a17a0aec29de1535223b9c6f60d106f4309
jimmy5211 <45889916+jimmy5211@users.noreply.github.com>
2026-03-15 21:42:40 -0600

    Create stroke_model.dart

---
+++ lib/models/stroke_model.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class StrokeModel {
+  final List<Offset> points;
+  final Color color;
+  final double strokeWidth;
+  final double opacity;
+  final StrokeType type;
+  final int layerId;
+
+  StrokeModel({
+    required this.points,
+    required this.color,
+    required this.strokeWidth,
+    required this.opacity,
+    required this.type,
+    required this.layerId,
+  });
+
+  StrokeModel copyWith({
+    List<Offset>? points,
+    Color? color,
+    double? strokeWidth,
+    double? opacity,
+    StrokeType? type,
+    int? layerId,
+  }) {
+    return StrokeModel(
+      points: points ?? this.points,
+      color: color ?? this.color,
+      strokeWidth: strokeWidth ?? this.strokeWidth,
+      opacity: opacity ?? this.opacity,
+      type: type ?? this.type,
+      layerId: layerId ?? this.layerId,
+    );
+  }
+}
+
+enum StrokeType {
+  liner,
+  shader,
+  dotwork,
+  fill,
+  eraser,
+}
