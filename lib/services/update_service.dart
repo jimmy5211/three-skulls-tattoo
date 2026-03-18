b8a04c1991252b020d7d35b2b9f88b9b3b4e73bf
jimmy5211 <45889916+jimmy5211@users.noreply.github.com>
2026-03-17 23:12:29 -0600

    Update update_service.dart

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -30,7 +30,7 @@ class UpdateService {
         Uri.parse(_versionUrl),
         headers: {
           'X-Master-Key':
-              r'$2a$10$Rj40cjH3ckcXtxo9CmUY9e9MUXgigpAOoQwLj9nhcTsD/QQZAdwM.',
+              $2a$10$FOj0uoW3syBnsFzUfq2P9ujG3wIwwTiERr9zVll9emK1RCIL6AvtG',
           'X-Bin-Meta': 'false',
         },
       ).timeout(const Duration(seconds: 15));
